ac0646b75460162bf06afa2a5d996a5f5bae6311
bitshitter <83315392+bitshitter@users.noreply.github.com>
2021-06-11 22:25:16 +0100

    Add LED on button press/hold

--- StateMachine.h
+++ StateMachine.h
@@ -1,71 +1,218 @@
-#ifndef STATE_MACHINE_H
-#define STATE_MACHINE_H
+#ifndef VOLUME_MOTOR_STATE_MACHINE_H
+#define VOLUME_MOTOR_STATE_MACHINE_H
 
-namespace StateMachineUtils
+#include "Arduino.h"
+#include "StateMachine.h"
+#include "IrReceiver.h"
+
+namespace VolumeMotorUtils
 {
-    template <class TStateId> class State
-    {
-        public:
-            /**
-             * @param deltaMicros The time (in microseconds) since the last tick of the state machine
-             * @return The new state of the state machine
-             */
-            virtual TStateId const Tick(unsigned long const deltaMicros) = 0;
+    using namespace IrReceiverUtils;
+    using namespace StateMachineUtils;
 
-            /**
-             * Called when the state machine enters this state.
-             * Should reset any internal state inside the State object.
-             * Should apply any associated external state (e.g. turning on an output pin),
-             * making no assumptions (e.g. do not assume that output pins are already LOW)
-             */
-            virtual void OnEnterState() = 0;
+    struct VolumeMotorConfig
+    {
+        // IR code to signal volume up command
+        unsigned long const VolumeUpCode;
+        // IR code to signal volume down command
+        unsigned long const VolumeDownCode;
+
+        // Digital output pin to drive motor in volume up direction
+        int const VolumeUpPin;
+        // Digital output pin to drive motor in volume down direction
+        int const VolumeDownPin;
+		// Digital output pin to light LED
+        int const LedPin;
+
+        // Duration to drive motor in brake mode (both inputs on) when stopping
+        unsigned long const BrakeDurationMicros;
+        // Duration to wait since last IR code before stopping
+        unsigned long const MovementTimeoutMicros;
     };
 
-    unsigned long const Duration(unsigned long const startMicros, unsigned long const endMicros)
+    enum MotorStateId
     {
-        if(startMicros > endMicros) return startMicros - endMicros;
-        else return endMicros - startMicros;
-    }
+        IDLE,
+        VOLUME_INCREASING,
+        VOLUME_DECREASING,
+        BRAKING
+    };
 
-    template <class TStateId> class StateMachine
+    class IdleMotorState : public State<MotorStateId>
     {
         private:
-            State<TStateId> * currentState;
-            unsigned long lastTickMicros = 0;
-            TStateId currentStateId;
+            IrReceiver & irReceiver;
+            VolumeMotorConfig const & config;
+
+        public:
+            IdleMotorState(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const & config)
+                : irReceiver(irReceiver)
+                , config(config)
+            { }
+
+            MotorStateId const Tick(unsigned long const)
+            {
+                IrPacket packet;
+                if (irReceiver.TryGetPacket(packet) && !packet.IsRepeat)
+                {
+                    if (packet.Code == config.VolumeUpCode) return VOLUME_INCREASING;
+                    else if (packet.Code == config.VolumeDownCode) return VOLUME_DECREASING;
+                }
+                return IDLE;
+            }
+
+            void OnEnterState()
+            {
+                digitalWrite(config.VolumeUpPin, LOW);
+                digitalWrite(config.VolumeDownPin, LOW);
+				digitalWrite(config.LedPin, LOW);
+            }
+    };
+
+    class BrakingMotorState : public State<MotorStateId>
+    {
+        private:
+            IrReceiver & irReceiver;
+            VolumeMotorConfig const & config;
+            unsigned long brakeTimeMicros = 0; // Time that motor has been braking for
+
+        public:
+            BrakingMotorState(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const & config)
+                : irReceiver(irReceiver)
+                , config(config)
+            { }
+
+            MotorStateId const Tick(unsigned long const deltaMicros)
+            {
+                if (irReceiver.TryGetPacket())
+                {
+                    // Use last code so that the motor will restart in its last direction if a repeat
+                    // packet was missed for some reason (often happens with poor quality demodulators)
+                    auto const code = irReceiver.GetLastCode();
+                    if (code == config.VolumeUpCode) return VOLUME_INCREASING;
+                    else if (code == config.VolumeDownCode) return VOLUME_DECREASING;
+                }
+                brakeTimeMicros += deltaMicros;
+                if(brakeTimeMicros >= config.BrakeDurationMicros) return IDLE;
+                else return BRAKING;
+            }
+
+            void OnEnterState()
+            {
+                brakeTimeMicros = 0;
+                digitalWrite(config.VolumeUpPin, HIGH);
+                digitalWrite(config.VolumeDownPin, HIGH);
+				digitalWrite(config.LedPin, HIGH);
+            }
+    };
+
+    template <bool const VolumeUp> class MovingMotorState : public State<MotorStateId>
+    {
+        private:
+            IrReceiver & irReceiver;
+            VolumeMotorConfig const & config;
+            unsigned long microsSinceLastForwardCommand = 0; // Time since last matching command/repeat packet
+
+            unsigned long const forwardCommandCode = VolumeUp ? config.VolumeUpCode: config.VolumeDownCode;
+            unsigned long const reverseCommandCode = VolumeUp ? config.VolumeDownCode : config.VolumeUpCode;
+            int const forwardPin = VolumeUp ? config.VolumeUpPin: config.VolumeDownPin;
+            int const reversePin = VolumeUp ? config.VolumeDownPin : config.VolumeUpPin;
+			int const lightPin = VolumeUp ? config.LedPin : config.LedPin;
+            static MotorStateId const forwardState = VolumeUp ? VOLUME_INCREASING : VOLUME_DECREASING;
+            static MotorStateId const reverseState = VolumeUp ? VOLUME_DECREASING : VOLUME_INCREASING;
+
+        public:
+            MovingMotorState(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const & config)
+                : irReceiver(irReceiver)
+                , config(config)
+            { }
+
+            MotorStateId const Tick(unsigned long const deltaMicros)
+            {
+                IrPacket packet;
+                if (irReceiver.TryGetPacket(packet))
+                {
+                    if (packet.IsRepeat || packet.Code == forwardCommandCode) microsSinceLastForwardCommand = 0;
+                    else if (packet.Code == reverseCommandCode) return reverseState;
+                }
+                else microsSinceLastForwardCommand += deltaMicros;
+
+                return microsSinceLastForwardCommand > config.MovementTimeoutMicros ? BRAKING : forwardState;
+            }
+
+            void OnEnterState()
+            {
+                microsSinceLastForwardCommand = 0;
+                // Setting the reverse pin to low first ensures that no braking occurs
+                digitalWrite(reversePin, LOW);
+                digitalWrite(forwardPin, HIGH);
+				digitalWrite(lightPin, HIGH);
+            }
+    };
+
+    class VolumeIncreasingMotorState : public MovingMotorState<true>
+    {
+        public:
+            VolumeIncreasingMotorState(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const & config)
+                : MovingMotorState(irReceiver, config)
+            { }
+    };
+
+    class VolumeDecreasingMotorState : public MovingMotorState<false>
+    {
+        public:
+            VolumeDecreasingMotorState(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const & config)
+                : MovingMotorState(irReceiver, config)
+            { }
+    };
+
+    class VolumeMotorStateMachine : public StateMachine<MotorStateId>
+    {
+        private:
+            IrReceiver & irReceiver;
+            VolumeMotorConfig const config;
+            VolumeIncreasingMotorState volumeIncreasingMotorState;
+            VolumeDecreasingMotorState volumeDecreasingMotorState;
+            BrakingMotorState brakingMotorState;
+            IdleMotorState idleMotorState;
 
         protected:
-            void SetState(TStateId const newStateId)
+            State<MotorStateId> * GetStateInstance(MotorStateId const stateId) const
             {
-                if (newStateId != currentStateId)
+                switch(stateId)
                 {
-                    currentState = GetStateInstance(newStateId);
-                    currentState->OnEnterState();
-                    currentStateId = newStateId;
+                    case VOLUME_INCREASING: return &volumeIncreasingMotorState;
+                    case VOLUME_DECREASING: return &volumeDecreasingMotorState;
+                    case BRAKING: return &brakingMotorState;
+                    case IDLE:
+                    default:
+                        return &idleMotorState;
                 }
             }
 
-            /**
-             * @param stateId An identifier representing a state
-             * @return Pointer to a state object corresponding with the given id
-             */
-            virtual State<TStateId> * GetStateInstance(TStateId const stateIdentifier) const = 0;
-
         public:
-            StateMachine(
-                TStateId const initialStateId,
-                State<TStateId> * currentState)
-                : currentStateId(initialStateId)
-                , currentState(currentState)
+            VolumeMotorStateMachine(
+                IrReceiver & irReceiver,
+                VolumeMotorConfig const && inConfig) // Called "inConfig" to distinguish it from the member "config" when initialising the states below
+                : StateMachine(IDLE, &idleMotorState)
+                , config(inConfig)
+                , irReceiver(irReceiver)
+                , volumeIncreasingMotorState(irReceiver, config)
+                , volumeDecreasingMotorState(irReceiver, config)
+                , brakingMotorState(irReceiver, config)
+                , idleMotorState(irReceiver, config)
             { }
-
-            void Tick()
-            {
-                auto const currentMicros = micros();
-                SetState(currentState->Tick(Duration(lastTickMicros, currentMicros)));
-                lastTickMicros = currentMicros;
-            }
     };
 }
 
-#endif //STATE_MACHINE_H
+#endif //VOLUME_MOTOR_STATE_MACHINE_H
