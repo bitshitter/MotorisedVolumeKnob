d31cc72b81e1afad66def9ca5a0b2a0a6efc9a1b
bitshitter <83315392+bitshitter@users.noreply.github.com>
2021-06-11 22:23:33 +0100

    Add LED on button press/hold

--- MotorisedVolumeKnob.ino
+++ MotorisedVolumeKnob.ino
@@ -7,12 +7,14 @@ using namespace VolumeMotorUtils;
 int const IR_RECV_PIN = 2;
 int const VOLUME_UP_PIN = 4;
 int const VOLUME_DOWN_PIN = 3;
+int const LED_PIN = 5;
 
 void setup()
 {
     pinMode(IR_RECV_PIN, INPUT);
     pinMode(VOLUME_UP_PIN, OUTPUT);
     pinMode(VOLUME_DOWN_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 auto & receiver = InputPinIrReceiver<IR_RECV_PIN>::Attach(/*inverted:*/true);
@@ -25,6 +27,7 @@ auto motorStateMachine = VolumeMotorStateMachine(
         .VolumeDownCode = 0xFFE01F,
         .VolumeUpPin = VOLUME_UP_PIN,
         .VolumeDownPin = VOLUME_DOWN_PIN,
+        .LedPin = LED_PIN,
         .BrakeDurationMicros = 100UL * 1000UL,
         .MovementTimeoutMicros = 120UL * 1000UL
     });
